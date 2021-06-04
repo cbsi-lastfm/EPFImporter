c977fbe9b8f609f255a047930c8a42430767d298
Ben Isaacs <75862+ben-xo@users.noreply.github.com>
2021-06-04 17:18:13 +0100

    Eh probably counterproductive in the producer  too

--- EPFParser.py
+++ EPFParser.py
@@ -118,7 +118,7 @@ class Parser(object):
                         q.append(ln)
                         break
                     except:
-                        time.sleep(0)  # pass but favour other threads
+                        pass
 
                 if not ln or len(ln) == 0 or ln[0] == "\x00":  # end of file - skipping zero-fill at the end of tarfile
                     break
